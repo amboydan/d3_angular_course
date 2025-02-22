52bd56445d7b355d1caf0bf8c115053272851d11
amboydan <amboydan@protonmail.com>
2025-02-21 20:11:13 -0900

    working through config of pie chart

--- src/app/charts/chart6/chart6.component.ts
+++ src/app/charts/chart6/chart6.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
+import { IPieData } from '../../interfaces/chart.interfaces';
 
 @Component({
   selector: 'app-chart6',
@@ -10,11 +11,28 @@ import * as d3 from 'd3';
   styleUrl: './chart6.component.scss',
   encapsulation: ViewEncapsulation.None
 })
+
 export class Chart6Component implements OnInit{
 
   host: any;
 
-  @Input() data;
+  @Input() data: IPieData;
+
+  config: any;
+
+  dimensions: DOMRect;
+
+  innerWidth: number;
+  innerHeight: number;
+  radius: number;
+  innerRadius = 0;
+
+  margins: {
+    top: 0, 
+    right: 0,
+    bottom: 0,
+    left: 0
+  };
 
   constructor(element: ElementRef) {
     this.host = d3.select(element.nativeElement);

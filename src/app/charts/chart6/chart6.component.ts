e191c6abae594d579ad779b9f04cbe3fa53f7ffa
amboydan <amboydan@protonmail.com>
2025-02-24 20:32:16 -0900

    almost there with pie

--- src/app/charts/chart6/chart6.component.ts
+++ src/app/charts/chart6/chart6.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, OnInit, OnChanges, SimpleChanges, ViewEncapsulation} from '@angular/core';
+import { Component, ElementRef, Input, OnInit, OnChanges, SimpleChanges, ViewEncapsulation, ɵsetCurrentInjector} from '@angular/core';
 import * as d3 from 'd3';
 import { IPieConfig, IPieData } from '../../interfaces/chart.interfaces';
+import { PieHelper } from '../../helpers/pie.helper';
 
 @Component({
   selector: 'app-chart6',
@@ -16,14 +17,23 @@ export class Chart6Component implements OnInit, OnChanges{
 
   host: any;
   svg: any;
+
+  // containers
   dataContainer: any;
   legendContainer: any;
+
   title: any;
 
+  // functions
   arc: any;
   pie: any;
-  colors: any;
   arcTween: any;
+
+  // scales
+  colors: any;
+
+  // state
+  hiddenIds = new Set(); // only need the id and don't need to store extra
 
   @Input() data: IPieData;
 
@@ -41,12 +51,12 @@ export class Chart6Component implements OnInit, OnChanges{
       stroke: '#fff',// white stroke
       strokeWidth: 2,
       radius: 6,
-      padAngle: 0
+      padAngle: .015
     },
     margins: {
       left:10,
       top: 40,
-      right: 130,
+      right: 50,
       bottom: 10
     }
   };
@@ -69,7 +79,8 @@ export class Chart6Component implements OnInit, OnChanges{
   }
 
   get pieData() {
-    return this.pie(this.data.data)
+    // only want the data if it belongs to not hidden.  doesn't belong to hidden id's set
+    return this.pie(this.data.data.filter((elem) => !this.hiddenIds.has(elem.id)));
   }
 
   constructor(element: ElementRef) {
@@ -84,8 +95,8 @@ export class Chart6Component implements OnInit, OnChanges{
     this.setElements();
     this.updateChart();
 
-    console.log(this);
-    console.log(d3);
+    //console.log(this);
+    //console.log(d3);
   }
 
   ngOnChanges (changes: SimpleChanges) {
@@ -136,24 +147,29 @@ export class Chart6Component implements OnInit, OnChanges{
     this.arc = d3.arc()
       .innerRadius(this.innerRadius)
       .outerRadius(this.radius)
+      .padAngle(this.config.arcs.padAngle)
+      .cornerRadius(this.config.arcs.radius);
     
     // pie generator
     this.pie = d3.pie()
-      .value((d) => d.value)
+      .value((d: any) => d.value)
+      .sort((a, b) => d3.ascending(a.id, b.id));
 
     // color scale
-    this.colors = d3.scaleOrdinal(d3.schemeCategory10)
-      .domain(this.ids)
+    this.colors = d3
+      .scaleOrdinal(d3.schemeCategory10)
+      .domain(this.ids as any);
 
     const chart = this;
 
     this.arcTween = function(d) {
-      console.log(this);
+      //console.log(this);
       const current = d;
-      const previous = this._previous; // if the previous state is available
+      const parent: any = this
+      const previous = parent._previous; // if the previous state is available
       const interpolate = d3.interpolate(previous, current);
       this._previous = current;
-      return function(t) {
+      return function(t: number) {
         return chart.arc(interpolate(t));
       }
     }
@@ -163,10 +179,78 @@ export class Chart6Component implements OnInit, OnChanges{
     this.title.text(this.data.title);
   }
 
-  setLegend() {}
+  setLegend() {
+    const data = this.data.data;
+
+    // add legend item containers
+    this.legendContainer.selectAll('g.legend-item')
+      .data(data)
+      .join('g')
+        .attr('class', 'legend-item')
+        .attr('transform', (d, i) => `translate(0, ${i * this.config.legendItem.height})`)
+        // set as disabled if the id belongs to the hidden ids
+        .style('opacity', (d) => this.hiddenIds.has(d.id) ? this.config.hiddenOpacity : null)
+        .on('mouseenter', (event, d) => this.setHighlight(d.id))
+        .on('mouseleave', (event, d) => this.resetHighlight())
+        .on('click', (event, d) => this.toggleHighlight(d.id));
+
+    // add symbols
+    this.legendContainer.selectAll('g.legend-item')
+      .selectAll('rect')
+      .data((d) => [d]) // need to return as an array
+      .join('rect')
+        .attr('width', this.config.legendItem.symbolSize)
+        .attr('height', this.config.legendItem.symbolSize)
+        .style('fill', (d) => this.colors(d.id));
+       
+    // add labels
+    this.legendContainer.selectAll('g.legend-item')
+      .selectAll('text')
+      .data((d) => [d])
+      .join('text')
+      .style('font-size', this.config.legendItem.fontSize + 'px')
+      .attr('x', this.config.legendItem.textSeparator)
+      .attr('y', this.config.legendItem.symbolSize)
+      .text((d) => d.label);
+      
+    // reposition legend
+    const dimensions = this.legendContainer.node().getBBox();
+
+    this.legendContainer
+      .attr('transform', `translate(${this.dimensions.width - this.margins.right}, ${this.margins.top + 0.5 * this.innerHeight - 0.5 * dimensions.height})`)
+  }
+
+  extendPreviousDataWithEnter = (previous, current) => {
+
+    const previousIds = new Set(previous.map((d) => d.data.id));
+    const beforeEndAngle = (id) => previous.find((d) => d.data.id === id)?.endAngle || 0; 
+    // get new elemnts (the enter selection)
+    // elements belonging to current that don't belong to previous
+    const newElements = current.filter((elem) => !previousIds.has(elem.data.id))
+      .map((elem) => {
+        const before = current.find((d) => d.index === elem.index -1);
+
+        // get end angle of the previous element in the prevoius data
+        const angle = beforeEndAngle(before?.data?.id) // if you don't have the ? on the first when doesn't exist you will get an error
+
+        return {
+          ...elem,
+          startAngle: angle,
+          endAngle: angle
+        };
+      });
+
+      return [...previous, ...newElements]
+  }
 
   draw() {
     const data = this.pieData;
+
+    const  previousData = this.dataContainer
+      .selectAll('path.data')
+      .data();
+
+    const extendedPreviousData = this.extendPreviousDataWithEnter(previousData, data);
     // Remember the update pattern: bind the data, enter / update, exit / remove
     // 1. Bind the data
     // const arcs = this.dataContainer
@@ -201,6 +285,8 @@ export class Chart6Component implements OnInit, OnChanges{
         // removeing the previous.  
         .attr('class', 'data')
         .style('fill', (d) => this.colors(d.data.id))
+        .style('stroke', this.config.arcs.stroke)
+        .style('stroke-width', this.config.arcs.strokeWidth)
         // github.com/d3/d3-transition: understand the strange transition
         // we need to creat a custom interporlator because currently it is a string
         // For us, we need to interpolate the start and end angle (d3-interpolate). 
@@ -211,7 +297,27 @@ export class Chart6Component implements OnInit, OnChanges{
         .attrTween('d', this.arcTween);
   }
 
-  highlight() {}
+  setHighlight(id) {
+    this.dataContainer.selectAll('path.data')
+    .style('opacity', (d) => d.data.id == id ? null : this.config.hiddenOpacity);
+
+    this.legendContainer.selectAll('g.legend-item')
+      .style('opacity', (d) => d.id === id ? null : this.config.hiddenOpacity);
+  }
+
+  resetHighlight() {
+    this.dataContainer.selectAll('path.data')
+    .style('opacity', null);
+
+    this.legendContainer.selectAll('g.legend-item')
+      .style('opacity', null);
+  }
+
+  toggleHighlight(id: any): void {
+    this.hiddenIds.has(id) ? this.hiddenIds.delete(id) : this.hiddenIds.add(id);
+    this.updateChart();
+    console.log(this.hiddenIds);
+  }
 
   updateChart() {
     this.setParams();
--- src/app/helpers/pie.helper.ts
+++ src/app/helpers/pie.helper.ts
@@ -1,19 +1,49 @@
-import { IPieData } from "../interfaces/chart.interfaces";
+import { IPieData } from '../interfaces/chart.interfaces';
+import * as d3 from 'd3';
 
 export class PieHelper {
-    // this method is "much more generic" and can be "reused" with other data sets
     static convert(data: any, title: string, valueAttr: string, idAttr: string, labelAttr: string): IPieData {
-        // (data || []) in order to make sure that the data will work if null or undefined then want 
-        // to replace it with an empty array
+
         const pieData = (data || []).map((elem: any) => ({
             id: elem[idAttr],
             label: elem[labelAttr],
-            value: elem[valueAttr]
-          }));
-      
-          return {
+            value: elem[valueAttr],
+        }));
+
+        return {
             title,
-            data: pieData
-          }
-    }   
+            data: pieData,
+        }
+    }
+
+    static ExtendPreviousDataWithEnter = (previous: any, current: any) => {
+        const previousIds = new Set(previous.map((d: any) => d.data.id));
+        const beforeEndAngle = (id: any) => previous.find((d: any) => d.data.id === id)?.endAngle || 0;
+        const newElements = current.filter((elem: any) => !previousIds.has(elem.data.id)).map((elem: any) => {
+            const before = current.find((d: any) => d.index === elem.index - 1);
+            const angle = beforeEndAngle(before?.data?.id);
+            return {
+                ...elem,
+                startAngle: angle,
+                endAngle: angle,
+            };
+        });
+        return [...previous, ...newElements];
+    }
+
+    static ExtendCurrentDataWithExit = (previous: any, current: any) => {
+        return PieHelper.ExtendPreviousDataWithEnter(current, previous);
+    }
+
+    static ArcTweenFactory = (data: any, enter: boolean, arc: any) => {
+        const arcTween = function (elementData: any) {
+            const previousElemData = data.find((d: any) => d.data.id === elementData.data.id);
+            const [start, end] = enter ? [previousElemData, elementData] : [elementData, previousElemData];
+            const interpolate = d3.interpolate(start, end);
+            return function (t: any) {
+                return arc(interpolate(t));
+            }
+        }
+        return arcTween;
+    }
 }

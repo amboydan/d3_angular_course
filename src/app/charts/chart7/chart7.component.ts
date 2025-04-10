a8fd466f018742ffd1ea09241a552fc9476a24d3
amboydan <amboydan@protonmail.com>
2025-04-09 19:21:36 -0800

    finished lesson 161 again.  this has been worth it. revisiting and checking where things went wrong

--- src/app/charts/chart7/chart7.component.ts
+++ src/app/charts/chart7/chart7.component.ts
@@ -25,8 +25,30 @@ export class Chart7Component implements OnInit, OnChanges{
   svg: any;
 
 @Input() data;
-@Input() config;
+@Input() set config(values) {
+  this._config = ObjectHelper.UpdateObjectWithPartialValues(this._defaultConfig, values);
+}
+
+get config() {
+  if (!this._config) {
+    this.config = this._defaultConfig;
+  }
+
+  return this._config;
+}
+
+private _config: IGroupStackConfig;
   
+private _defaultConfig: IGroupStackConfig = {
+      hiddenOpacity: 0.3,
+      transition: 300,
+      margins: {
+        top: 40,
+        right: 20,
+        bottom: 60,
+        left: 50
+      }
+}
 
   constructor(element: ElementRef) {
     this.host = d3.select(element.nativeElement);
@@ -34,14 +56,37 @@ export class Chart7Component implements OnInit, OnChanges{
   }
 
   ngOnInit(): void {
+    this.svg = this.host.select('svg');
 
+    this.setDimensions();
+    this.setElements();
+    this.updateChart();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (!this.svg) { return; }
 
+    this.updateChart();
   }
 
-  
+  setDimensions(): void {}
+  setElements(): void {}
+  setParams(): void {}
+  setLabels(): void {}
+  setAxis(): void {}
+  setLegend(): void {}
+  draw(): void {}
 
+  updateChart() {
+    this.setParams(); 
+    this.setLabels();
+    this.setAxis();
+    this.setLegend(); 
+    this.draw();
+  }
+
+  // tooltip
+
+  // highlight
 
 }

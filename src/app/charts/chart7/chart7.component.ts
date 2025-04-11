b3cf4ee940bb765d90c12668573c112585d578c1
amboydan <amboydan@protonmail.com>
2025-04-10 17:31:47 -0800

    moving along

--- src/app/charts/chart7/chart7.component.ts
+++ src/app/charts/chart7/chart7.component.ts
@@ -24,6 +24,24 @@ export class Chart7Component implements OnInit, OnChanges{
   host: any;
   svg: any;
 
+  dimensions: ChartDimensions;
+
+  // axis
+  xAxis: any;
+  yAxis: any;
+
+  // containers
+  xAxisContainer: any;
+  yAxisContainer: any;
+  dataContainer: any;
+  legendContainer: any;
+  tooltipContainer: any;
+
+  // labels
+  title: any;
+  yLabel: any;
+
+
 @Input() data;
 @Input() set config(values) {
   this._config = ObjectHelper.UpdateObjectWithPartialValues(this._defaultConfig, values);
@@ -69,8 +87,36 @@ private _defaultConfig: IGroupStackConfig = {
     this.updateChart();
   }
 
-  setDimensions(): void {}
-  setElements(): void {}
+  setDimensions(): void {
+    this.dimensions = new ChartDimensions(this.svg.node().getBoundingClientRect(), this.config.margins);
+  }
+  setElements(): void {
+    this.xAxisContainer = this.svg.append('g').attr('class', 'xAxisContainer')
+      .attr('transform', `translate(${this.dimensions.marginLeft}, ${this.dimensions.marginBottom})`);
+
+    this.yAxisContainer = this.svg.append('g').attr('class', 'yAxisContainer')
+      .attr('transform', `translate(${this.dimensions.marginLeft}, ${this.dimensions.marginTop})`);
+
+    this.dataContainer = this.svg.append('g').attr('class', 'dataContainer')
+      .attr('transform', `translate(${this.dimensions.marginLeft}, ${this.dimensions.marginTop})`);
+
+    this.legendContainer = this.svg.append('g').attr('class', 'legendContainer')
+      .attr('transform', `translate(${this.dimensions.marginLeft}, ${this.dimensions.marginBottom + 30})`);
+
+    this.title = this.svg.append('g').attr('class', 'titleContainer')
+      .attr('transform', `translate(${this.dimensions.midWidth}, ${this.dimensions.midMarginTop})`)
+      .append('text').attr('class', 'title')
+      .style('text-anchor', 'middle');
+
+    this.yLabel = this.svg.append('g').attr('class', 'yLabelContainer')
+      .attr('transform', `translate(${this.dimensions.marginLeft - 30}, ${this.dimensions.midMarginTop})`)
+      .append('text').attr('class', 'yLabel')
+      .style('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)');
+
+    //tooltip
+    
+  }
   setParams(): void {}
   setLabels(): void {}
   setAxis(): void {}
